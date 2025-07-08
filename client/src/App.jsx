a7a0831ecb4dce5ce8b7a844d0a53013682acd00
Yuvraj <yuvrajsingh2k2@gmail.com>
2025-07-08 12:14:32 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,54 @@
-import { useState } from 'react'
-import BlogForm from './components/BlogForm'
-import BlogList from './components/BlogList'
-import Header from './components/Header'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import BlogForm from "./components/BlogForm";
+import BlogList from "./components/BlogList";
+import Header from "./components/Header";
+import "./App.css";
 
 function App() {
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
+
+  // Fetch previously posted blogs on first load
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/blogs")
+      .then((res) => {
+        const serverPosts = res.data.map((post) => ({
+          id: post._id, // mapping MongoDB _id to id
+          title: post.title,
+          content: post.content,
+          tags: post.tags || [],
+          date: new Date(post.date).toLocaleString(),
+        }));
+        setPosts(serverPosts);
+      })
+      .catch((err) => console.error("Error fetching posts:", err));
+  }, []);
 
   const addPost = (title, content, tags) => {
-    const newPost = {
-      id: Date.now(),
-      title,
-      content,
-      tags,
-      date: new Date().toLocaleString()
-    }
-    setPosts([newPost, ...posts])
-  }
+    axios
+      .post("http://localhost:5000/api/blogs", { title, content, tags })
+      .then((res) => {
+        const newPost = {
+          id: res.data._id, // from MongoDB
+          title: res.data.title,
+          content: res.data.content,
+          tags: res.data.tags || [],
+          date: new Date(res.data.date).toLocaleString(),
+        };
+        setPosts([newPost, ...posts]);
+      })
+      .catch((err) => console.error("Error adding post:", err));
+  };
 
   const deletePost = (id) => {
-    setPosts(posts.filter(p => p.id !== id))
-  }
+    axios
+      .delete(`http://localhost:5000/api/blogs/${id}`)
+      .then(() => {
+        setPosts(posts.filter((p) => p.id !== id));
+      })
+      .catch((err) => console.error("Error deleting post:", err));
+  };
 
   return (
     <div className="app-container">
@@ -30,7 +58,7 @@ function App() {
         <BlogList posts={posts} deletePost={deletePost} />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -6,7 +6,10 @@ require('dotenv').config();
 const blogRoutes = require('./routes/blogRoutes');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
 app.use(express.json());
 
 app.use('/api/blogs', blogRoutes);
